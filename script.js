04bf762b2a42b6bcbe827f483572d2188e0ef665
Kyoung <a01023906731@gmail.com>
2024-06-15 13:24:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function addWord(event) {
     // 단어 목록에 추가
     const li = document.createElement('li');
     li.textContent = `${word} - ${meaning}`;
-    wordList.insertBefore(li, wordList.firstChild); // 목록 위로 추가
+    wordList.appendChild(li);
 
     wordInput.value = '';
     meaningInput.value = '';
